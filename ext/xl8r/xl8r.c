23598edd308e81ce41f1088f66b66ee1d449a563
Evan Phoenix <evan@fallingsnow.net>
2012-07-12 19:49:11 -0700

    Add ability to install custom func for any method

--- ext/xl8r/xl8r.c
+++ ext/xl8r/xl8r.c
@@ -548,6 +548,297 @@ call_cfunc(func, recv, len, argc, argv)
   return Qnil;		/* not reached */
 }
 
+VALUE spec_no_args(VALUE klass, VALUE recv, ID id, ID oid,
+                          int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  int state;
+  VALUE *local_vars;	/* OK */
+  NODE *saved_cref = 0;
+  NODE* b2;
+  volatile int safe = -1;
+  VALUE result;
+
+  PUSH_SCOPE();
+
+  if (body->nd_rval) {
+    saved_cref = ruby_cref;
+    ruby_cref = (NODE*)body->nd_rval;
+  }
+
+  PUSH_CLASS(ruby_cbase);
+
+  if (body->nd_tbl) {
+    local_vars = TMP_ALLOC(body->nd_tbl[0]+1);
+    *local_vars++ = (VALUE)body;
+    rb_mem_clear(local_vars, body->nd_tbl[0]);
+    ruby_scope->local_tbl = body->nd_tbl;
+    ruby_scope->local_vars = local_vars;
+  } else {
+    local_vars = ruby_scope->local_vars = 0;
+    ruby_scope->local_tbl  = 0;
+  }
+
+  b2 = body = body->nd_next;
+
+  if (NOEX_SAFE(flags) > ruby_safe_level) {
+    safe = ruby_safe_level;
+    ruby_safe_level = NOEX_SAFE(flags);
+  }
+
+  PUSH_VARS();
+  PUSH_TAG(PROT_FUNC);
+
+  if ((state = EXEC_TAG()) == 0) {
+    if (argc > 0) {
+      rb_raise(rb_eArgError, "wrong number of arguments (%d for %d)",
+          argc, 0);
+    }
+
+    ruby_frame->argc = 0;
+
+    body = body->nd_next;
+
+    if (event_hooks) {
+      EXEC_EVENT_HOOK(RUBY_EVENT_CALL, b2, recv, id, klass);
+    }
+
+    result = mri_eval(recv, body);
+  } else if (state == TAG_RETURN && TAG_DST()) {
+    result = prot_tag->retval;
+    state = 0;
+  }
+
+  POP_TAG();
+
+  if (event_hooks) {
+    EXEC_EVENT_HOOK(RUBY_EVENT_RETURN, ruby_current_node,
+        recv, id, klass);
+  }
+
+  POP_VARS();
+  POP_CLASS();
+  POP_SCOPE();
+
+  ruby_cref = saved_cref;
+
+  if (safe >= 0) ruby_safe_level = safe;
+
+  switch (state) {
+    case 0:
+      break;
+
+    case TAG_BREAK:
+    case TAG_RETURN:
+      JUMP_TAG(state);
+      break;
+
+    case TAG_RETRY:
+      if (rb_block_given_p()) JUMP_TAG(state);
+      /* fall through */
+    default:
+      jump_tag_but_local_jump(state, result);
+      break;
+  }
+
+  return result;
+}
+
+const char* node_name(NODE* n) {
+  if(!n) return "<NULL>";
+
+  switch(nd_type(n)) {
+    case NODE_METHOD: return "NODE_METHOD";
+    case NODE_FBODY: return "NODE_FBODY";
+    case NODE_CFUNC: return "NODE_CFUNC";
+    case NODE_SCOPE: return "NODE_SCOPE";
+    case NODE_BLOCK: return "NODE_BLOCK";
+    case NODE_IF: return "NODE_IF";
+    case NODE_CASE: return "NODE_CASE";
+    case NODE_WHEN: return "NODE_WHEN";
+    case NODE_OPT_N: return "NODE_OPT_N";
+    case NODE_WHILE: return "NODE_WHILE";
+    case NODE_UNTIL: return "NODE_UNTIL";
+    case NODE_ITER: return "NODE_ITER";
+    case NODE_FOR: return "NODE_FOR";
+    case NODE_BREAK: return "NODE_BREAK";
+    case NODE_NEXT: return "NODE_NEXT";
+    case NODE_REDO: return "NODE_REDO";
+    case NODE_RETRY: return "NODE_RETRY";
+    case NODE_BEGIN: return "NODE_BEGIN";
+    case NODE_RESCUE: return "NODE_RESCUE";
+    case NODE_RESBODY: return "NODE_RESBODY";
+    case NODE_ENSURE: return "NODE_ENSURE";
+    case NODE_AND: return "NODE_AND";
+    case NODE_OR: return "NODE_OR";
+    case NODE_NOT: return "NODE_NOT";
+    case NODE_MASGN: return "NODE_MASGN";
+    case NODE_LASGN: return "NODE_LASGN";
+    case NODE_DASGN: return "NODE_DASGN";
+    case NODE_DASGN_CURR: return "NODE_DASGN_CURR";
+    case NODE_GASGN: return "NODE_GASGN";
+    case NODE_IASGN: return "NODE_IASGN";
+    case NODE_CDECL: return "NODE_CDECL";
+    case NODE_CVASGN: return "NODE_CVASGN";
+    case NODE_CVDECL: return "NODE_CVDECL";
+    case NODE_OP_ASGN1: return "NODE_OP_ASGN1";
+    case NODE_OP_ASGN2: return "NODE_OP_ASGN2";
+    case NODE_OP_ASGN_AND: return "NODE_OP_ASGN_AND";
+    case NODE_OP_ASGN_OR: return "NODE_OP_ASGN_OR";
+    case NODE_CALL: return "NODE_CALL";
+    case NODE_FCALL: return "NODE_FCALL";
+    case NODE_VCALL: return "NODE_VCALL";
+    case NODE_SUPER: return "NODE_SUPER";
+    case NODE_ZSUPER: return "NODE_ZSUPER";
+    case NODE_ARRAY: return "NODE_ARRAY";
+    case NODE_ZARRAY: return "NODE_ZARRAY";
+    case NODE_HASH: return "NODE_HASH";
+    case NODE_RETURN: return "NODE_RETURN";
+    case NODE_YIELD: return "NODE_YIELD";
+    case NODE_LVAR: return "NODE_LVAR";
+    case NODE_DVAR: return "NODE_DVAR";
+    case NODE_GVAR: return "NODE_GVAR";
+    case NODE_IVAR: return "NODE_IVAR";
+    case NODE_CONST: return "NODE_CONST";
+    case NODE_CVAR: return "NODE_CVAR";
+    case NODE_NTH_REF: return "NODE_NTH_REF";
+    case NODE_BACK_REF: return "NODE_BACK_REF";
+    case NODE_MATCH: return "NODE_MATCH";
+    case NODE_MATCH2: return "NODE_MATCH2";
+    case NODE_MATCH3: return "NODE_MATCH3";
+    case NODE_LIT: return "NODE_LIT";
+    case NODE_STR: return "NODE_STR";
+    case NODE_DSTR: return "NODE_DSTR";
+    case NODE_XSTR: return "NODE_XSTR";
+    case NODE_DXSTR: return "NODE_DXSTR";
+    case NODE_EVSTR: return "NODE_EVSTR";
+    case NODE_DREGX: return "NODE_DREGX";
+    case NODE_DREGX_ONCE: return "NODE_DREGX_ONCE";
+    case NODE_ARGS: return "NODE_ARGS";
+    case NODE_ARGSCAT: return "NODE_ARGSCAT";
+    case NODE_ARGSPUSH: return "NODE_ARGSPUSH";
+    case NODE_SPLAT: return "NODE_SPLAT";
+    case NODE_TO_ARY: return "NODE_TO_ARY";
+    case NODE_SVALUE: return "NODE_SVALUE";
+    case NODE_BLOCK_ARG: return "NODE_BLOCK_ARG";
+    case NODE_BLOCK_PASS: return "NODE_BLOCK_PASS";
+    case NODE_DEFN: return "NODE_DEFN";
+    case NODE_DEFS: return "NODE_DEFS";
+    case NODE_ALIAS: return "NODE_ALIAS";
+    case NODE_VALIAS: return "NODE_VALIAS";
+    case NODE_UNDEF: return "NODE_UNDEF";
+    case NODE_CLASS: return "NODE_CLASS";
+    case NODE_MODULE: return "NODE_MODULE";
+    case NODE_SCLASS: return "NODE_SCLASS";
+    case NODE_COLON2: return "NODE_COLON2";
+    case NODE_COLON3: return "NODE_COLON3";
+    case NODE_CREF: return "NODE_CREF";
+    case NODE_DOT2: return "NODE_DOT2";
+    case NODE_DOT3: return "NODE_DOT3";
+    case NODE_FLIP2: return "NODE_FLIP2";
+    case NODE_FLIP3: return "NODE_FLIP3";
+    case NODE_ATTRSET: return "NODE_ATTRSET";
+    case NODE_SELF: return "NODE_SELF";
+    case NODE_NIL: return "NODE_NIL";
+    case NODE_TRUE: return "NODE_TRUE";
+    case NODE_FALSE: return "NODE_FALSE";
+    case NODE_DEFINED: return "NODE_DEFINED";
+    case NODE_NEWLINE: return "NODE_NEWLINE";
+    case NODE_POSTEXE: return "NODE_POSTEXE";
+    case NODE_ALLOCA: return "NODE_ALLOCA";
+    case NODE_DMETHOD: return "NODE_DMETHOD";
+    case NODE_BMETHOD: return "NODE_BMETHOD";
+    case NODE_MEMO: return "NODE_MEMO";
+    case NODE_IFUNC: return "NODE_IFUNC";
+    case NODE_DSYM: return "NODE_DSYM";
+    case NODE_ATTRASGN: return "NODE_ATTRASGN";
+    case NODE_LAST: return "NODE_LAST";
+
+
+    default: return "UNKNOWN";
+  }
+}
+
+VALUE spec_nil(VALUE klass, VALUE recv, ID id, ID oid,
+                          int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  return Qnil;
+}
+
+VALUE spec_true(VALUE klass, VALUE recv, ID id, ID oid,
+                          int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  return Qtrue;
+}
+
+VALUE spec_false(VALUE klass, VALUE recv, ID id, ID oid,
+                          int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  return Qfalse;
+}
+
+VALUE spec_self(VALUE klass, VALUE recv, ID id, ID oid,
+                          int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  return recv;
+}
+
+VALUE spec_lit(VALUE klass, VALUE recv, ID id, ID oid,
+                          int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  return body->nd_next->nd_next->nd_head->nd_next->nd_lit;
+}
+
+static NODE* specialize(NODE* scope) {
+  NODE* blk = scope->nd_next;
+  NODE* args = 0;
+
+  if(nd_type(blk) != NODE_BLOCK) return blk;
+
+  args = blk->nd_head;
+
+  if(nd_type(args) != NODE_ARGS) return blk;
+
+  if(args->nd_cnt == 0 && !args->nd_rest && !args->nd_opt) {
+    if(nd_type(blk->nd_next) == NODE_BLOCK &&
+        nd_type(blk->nd_next->nd_head) == NODE_NEWLINE &&
+        !blk->nd_next->nd_next) {
+
+      switch(nd_type(blk->nd_next->nd_head->nd_next)) {
+      case NODE_NIL:
+        blk->flags |= FL_FREEZE;
+        blk->u2.value = (VALUE)spec_nil;
+        break;
+      case NODE_TRUE:
+        blk->flags |= FL_FREEZE;
+        blk->u2.value = (VALUE)spec_true;
+        break;
+      case NODE_FALSE:
+        blk->flags |= FL_FREEZE;
+        blk->u2.value = (VALUE)spec_false;
+        break;
+      case NODE_SELF:
+        blk->flags |= FL_FREEZE;
+        blk->u2.value = (VALUE)spec_self;
+        break;
+      case NODE_LIT:
+        blk->flags |= FL_FREEZE;
+        blk->u2.value = (VALUE)spec_lit;
+        break;
+      default:
+        blk->flags |= FL_FREEZE;
+        blk->u2.value = (VALUE)spec_no_args;
+      }
+    } else {
+      blk->flags |= FL_FREEZE;
+      blk->u2.value = (VALUE)spec_no_args;
+    }
+  }
+
+  blk->flags |= FL_TAINT;
+
+  return blk;
+}
+
 static NODE* setup_args(NODE* body, VALUE recv,
                        int argc, VALUE* argv, VALUE* local_vars)
 {
@@ -557,11 +848,11 @@ static NODE* setup_args(NODE* body, VALUE recv,
   if (nd_type(body) == NODE_ARGS) {
     node = body;
     body = 0;
-  }
-  else if (nd_type(body) == NODE_BLOCK) {
+  } else if (nd_type(body) == NODE_BLOCK) {
     node = body->nd_head;
     body = body->nd_next;
   }
+
   if (node) {
     if (nd_type(node) != NODE_ARGS) {
       rb_bug("no argument-node");
@@ -631,6 +922,104 @@ static NODE* setup_args(NODE* body, VALUE recv,
   return body;
 }
 
+static VALUE generic_call(VALUE klass, VALUE recv, ID id, ID oid,
+                 int argc, VALUE* argv, NODE* volatile body, int flags)
+{
+  volatile VALUE result = Qnil;
+  int itr;
+  volatile int safe = -1;
+  NODE *b2;
+  TMP_PROTECT;
+
+  int state;
+  VALUE *local_vars;	/* OK */
+  NODE *saved_cref = 0;
+
+  PUSH_SCOPE();
+
+  if (body->nd_rval) {
+    saved_cref = ruby_cref;
+    ruby_cref = (NODE*)body->nd_rval;
+  }
+
+  PUSH_CLASS(ruby_cbase);
+
+  if (body->nd_tbl) {
+    local_vars = TMP_ALLOC(body->nd_tbl[0]+1);
+    *local_vars++ = (VALUE)body;
+    rb_mem_clear(local_vars, body->nd_tbl[0]);
+    ruby_scope->local_tbl = body->nd_tbl;
+    ruby_scope->local_vars = local_vars;
+  } else {
+    local_vars = ruby_scope->local_vars = 0;
+    ruby_scope->local_tbl  = 0;
+  }
+
+  if(body->nd_next->flags & FL_TAINT) {
+    b2 = body = body->nd_next;
+  } else {
+    b2 = body = specialize(body);
+  }
+
+  if (NOEX_SAFE(flags) > ruby_safe_level) {
+    safe = ruby_safe_level;
+    ruby_safe_level = NOEX_SAFE(flags);
+  }
+
+  PUSH_VARS();
+  PUSH_TAG(PROT_FUNC);
+
+  if ((state = EXEC_TAG()) == 0) {
+    body = setup_args(body, recv, argc, argv, local_vars);
+
+    if (event_hooks) {
+      EXEC_EVENT_HOOK(RUBY_EVENT_CALL, b2, recv, id, klass);
+    }
+
+    result = mri_eval(recv, body);
+  } else if (state == TAG_RETURN && TAG_DST()) {
+    result = prot_tag->retval;
+    state = 0;
+  }
+
+  POP_TAG();
+
+  if (event_hooks) {
+    EXEC_EVENT_HOOK(RUBY_EVENT_RETURN, ruby_current_node,
+        recv, id, klass);
+  }
+
+  POP_VARS();
+  POP_CLASS();
+  POP_SCOPE();
+
+  ruby_cref = saved_cref;
+
+  if (safe >= 0) ruby_safe_level = safe;
+
+  switch (state) {
+    case 0:
+      break;
+
+    case TAG_BREAK:
+    case TAG_RETURN:
+      JUMP_TAG(state);
+      break;
+
+    case TAG_RETRY:
+      if (rb_block_given_p()) JUMP_TAG(state);
+      /* fall through */
+    default:
+      jump_tag_but_local_jump(state, result);
+      break;
+  }
+
+  return result;
+}
+
+typedef VALUE (*caller)(VALUE klass, VALUE recv, ID id, ID oid,
+                 int argc, VALUE* argv, NODE* volatile body, int flags);
+
 VALUE xl8r_call0(VALUE klass, VALUE recv, ID id, ID oid,
                  int argc, VALUE* argv, NODE* volatile body, int flags)
 {
@@ -680,7 +1069,14 @@ VALUE xl8r_call0(VALUE klass, VALUE recv, ID id, ID oid,
   ruby_frame->argc = argc;
   ruby_frame->flags = 0;
 
-  switch (nd_type(body)) {
+  if(nd_type(body) == NODE_SCOPE &&
+      nd_type(body->nd_next) == NODE_BLOCK &&
+      body->nd_next->flags & FL_FREEZE)
+  {
+    caller c = (caller)body->nd_next->u2.value;
+    result = c(klass, recv, id, oid, argc, argv, body, flags);
+  } else {
+    switch (nd_type(body)) {
     case NODE_CFUNC:
       {
         int len = body->nd_argc;
@@ -747,91 +1143,16 @@ VALUE xl8r_call0(VALUE klass, VALUE recv, ID id, ID oid,
       break;
 
     case NODE_SCOPE:
-      {
-        int state;
-        VALUE *local_vars;	/* OK */
-        NODE *saved_cref = 0;
-
-        PUSH_SCOPE();
-
-        if (body->nd_rval) {
-          saved_cref = ruby_cref;
-          ruby_cref = (NODE*)body->nd_rval;
-        }
-
-        PUSH_CLASS(ruby_cbase);
-
-        if (body->nd_tbl) {
-          local_vars = TMP_ALLOC(body->nd_tbl[0]+1);
-          *local_vars++ = (VALUE)body;
-          rb_mem_clear(local_vars, body->nd_tbl[0]);
-          ruby_scope->local_tbl = body->nd_tbl;
-          ruby_scope->local_vars = local_vars;
-        } else {
-          local_vars = ruby_scope->local_vars = 0;
-          ruby_scope->local_tbl  = 0;
-        }
-
-        b2 = body = body->nd_next;
-
-        if (NOEX_SAFE(flags) > ruby_safe_level) {
-          safe = ruby_safe_level;
-          ruby_safe_level = NOEX_SAFE(flags);
-        }
-
-        PUSH_VARS();
-        PUSH_TAG(PROT_FUNC);
-
-        if ((state = EXEC_TAG()) == 0) {
-          body = setup_args(body, recv, argc, argv, local_vars);
-
-          if (event_hooks) {
-            EXEC_EVENT_HOOK(RUBY_EVENT_CALL, b2, recv, id, klass);
-          }
-          result = mri_eval(recv, body);
-        } else if (state == TAG_RETURN && TAG_DST()) {
-          result = prot_tag->retval;
-          state = 0;
-        }
-
-        POP_TAG();
-
-        if (event_hooks) {
-          EXEC_EVENT_HOOK(RUBY_EVENT_RETURN, ruby_current_node, recv, id, klass);
-        }
-
-        POP_VARS();
-        POP_CLASS();
-        POP_SCOPE();
-
-        ruby_cref = saved_cref;
-
-        if (safe >= 0) ruby_safe_level = safe;
-
-        switch (state) {
-          case 0:
-            break;
-
-          case TAG_BREAK:
-          case TAG_RETURN:
-            JUMP_TAG(state);
-            break;
-
-          case TAG_RETRY:
-            if (rb_block_given_p()) JUMP_TAG(state);
-            /* fall through */
-          default:
-            jump_tag_but_local_jump(state, result);
-            break;
-        }
-      }
+      result = generic_call(klass, recv, id, oid, argc, argv, body, flags);
       break;
 
     default:
       rb_bug("Unknown node used as method");
       break;
+    }
   }
 
+cleanup:
   POP_FRAME();
   POP_ITER();
   return result;
@@ -863,16 +1184,7 @@ int diff = 0x6149;
 void Init_xl8r_ext() {
   void* addr = dlsym(RTLD_DEFAULT, "rb_apply");
 
-  printf("appl = %p\n", addr);
-  /*
-
-  void *a2 = (void*)(((long)addr) - diff);
-
-
-  */
-
   void* call0 = APPLY_OFFSET(addr, OFFSET_CALL0);
-  printf("call0 = %p\n", call0);
 
   int_ruby_iter = APPLY_OFFSET(addr, OFFSET_RUBY_ITER);
   int_frame_unique = APPLY_OFFSET(addr, OFFSET_FRAME_UNIQUE);
@@ -896,4 +1208,7 @@ void Init_xl8r_ext() {
   buf[3] = ((dest >> 8)  & 0xff);
   buf[4] = ((dest >> 16) & 0xff);
   buf[5] = ((dest >> 24) & 0xff);
+
+  mprotect((void*)(((uintptr_t)buf) & ~0xfff), 4096,
+           PROT_READ | PROT_EXEC);
 }
